f5b491de8b6b612b905ad90452c9d5a4eba1b100
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-10-01 23:49:51 -0500

    Implements the functionality of expenses filter

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,9 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:category_id].present?
+      @expenses = Expense.where('concept like ? AND category_id = ?', "%#{params[:concept]}%", params[:category_id]).order('date DESC')
+    else
+      @expenses = Expense.where('concept like ?', "%#{params[:concept]}%").order('date DESC')
+    end
   end
 end
