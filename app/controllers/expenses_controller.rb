f590aabca5de8fc06ed00f1cd5673114ebe1e504
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-10-02 10:39:49 -0500

    Set expenses per user

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,9 +3,9 @@ class ExpensesController < ApplicationController
 
   def index
     if params[:category_id].present?
-      @expenses = Expense.where('concept like ? AND category_id = ?', "%#{params[:concept]}%", params[:category_id]).order('date DESC')
+      @expenses = current_user.expenses.where('concept like ? AND category_id = ?', "%#{params[:concept]}%", params[:category_id]).order('date DESC')
     else
-      @expenses = Expense.where('concept like ?', "%#{params[:concept]}%").order('date DESC')
+      @expenses = current_user.expenses.where('concept like ?', "%#{params[:concept]}%").order('date DESC')
     end
   end
 end
